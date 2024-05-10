9c8e43e79ad185a7509a96cb12057de46c6a086c
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2024-05-11 00:49:10 +0300

    Add random util

---
+++ backend/tests/random.js
@@ -0,0 +1,4 @@
+import { generateAccessToken } from '../utils/random.js';
+
+const accessToken = generateAccessToken(20);
+console.log(accessToken);
---
+++ backend/utils/random.js
@@ -0,0 +1,5 @@
+import crypto from 'crypto';
+
+export const generateAccessToken = (length) => {
+  return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+};
