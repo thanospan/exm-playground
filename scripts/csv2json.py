fff4498389ffe0ad99bea90a33634059889029da
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2024-05-11 20:00:34 +0300

    Update csv2json.py
    Change output directory

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -2,7 +2,7 @@ import csv
 import json
 
 csvfile = open('../data.csv', 'r')
-jsonfile = open('data.json', 'w')
+jsonfile = open('../data.json', 'w')
 
 fieldnames = ("timestamp","temperature","humidity","pressure","wind_speed","wind_gust","wind_direction")
 reader = csv.DictReader(csvfile, fieldnames)
