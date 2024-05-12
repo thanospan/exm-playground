05a28b8700e5d4b568c7fb74e224ef8d38066029
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2024-05-12 13:31:22 +0300

    Update client to read from csv file and send measurements to backend

--- client/app.js
+++ client/app.js
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+import fs from 'fs';
+import { parse } from 'csv-parse';
 
 dotenv.config({ path: './config/.env' });
 
@@ -39,40 +41,60 @@ const createDevice = async () => {
   }
 };
 
-const uploadTelemetry = async (entityId) => {
+const waitMillis = async (millis) => {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  return;
+};
+
+const sendMeasurements = async (entityId, measurements) => {
+  const uri = `http://${BACKEND_HOST}:${BACKEND_PORT}/devices/${entityId}/timeseries`;
+  const response = await fetch(uri, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(measurements)
+  });
+  const responseData = await response.json();
+  console.log(responseData);
+};
+
+const processFile = async (filepath, entityId) => {
+  const records = [];
+
+  const parser = fs
+    .createReadStream(filepath)
+    .pipe(parse({
+      delimiter: ",",
+      columns: true,
+      skip_empty_lines: true
+    }));
+  
+  for await (const record of parser) {
+    const measurement = {
+      ts: parseInt(record['timestamp']),
+      values: record
+    };
+    delete measurement.values['timestamp'];
+    records.push(measurement);
+
+    // 3600000 milliseconds in 1 hour
+    // 16000 milliseconds interval
+    // 225 records in 1 hour
+    if (records.length === 225) {
+      await waitMillis(1000);
+      await sendMeasurements(entityId, records);
+      records.length = 0;
+    }
+  }
+  return records;
+};
+
+(async () => {
   try {
-    const uri = `http://${BACKEND_HOST}:${BACKEND_PORT}/devices/${entityId}/timeseries`;
-
-    const headers = {
-      'Content-Type': 'application/json'
-    };
-
-    const body = {
-      "ts": 1648919852000,
-      "values": {
-        "temperature": 18.29999924,
-        "humidity": 52,
-        "pressure": 1008.74939,
-        "wind_speed": 0.128800005,
-        "wind_gust": 0.50999999,
-        "wind_direction": 80
-      }
-    };
-    
-    const response = await fetch(uri, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(body)
-    });
-
-    return await response.json();
+    const createDeviceResponse = await createDevice();
+    console.log(createDeviceResponse);
+  
+    await processFile('../data.csv', createDeviceResponse.response.id.id);
   } catch (error) {
     console.error(error);
   }
-};
-
-const createDeviceResponse = await createDevice();
-console.log(createDeviceResponse);
-
-const uploadTelemetryResponse = await uploadTelemetry(createDeviceResponse.response.id.id);
-console.log(uploadTelemetryResponse);
+})();
